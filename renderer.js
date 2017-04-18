ecf516b9c2ddc49af3a239028b8290a407c4504f
Maksim Ryzhikov <rv.maksim@gmail.com>
2017-04-18 23:39:52 +0300

    chore(react): electron + webpack + react

--- renderer.js
+++ renderer.js
@@ -1,7 +1,28 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-console.log('We are using Node.js ', process.versions.node);
-console.log('Chromium ', process.versions.chrome);
-console.log('Electron ', process.versions.electron);
-console.log('and Webpack ', require('webpack/package.json').version);
+const React = require('react');
+const e = React.createElement;
+const ReactDOM = require('react-dom');
+
+
+const App = function() {
+  return e('div', null,
+    e('div', null, 'We are using Node.js ', process.versions.node),
+    e('div', null, 'Chromium ', process.versions.chrome),
+    e('div', null, 'Electron ', process.versions.electron),
+    e('div', null, 'Webpack ', require('webpack/package.json').version),
+    e('div', null, 'and React ', require('react').version)
+  )
+};
+
+
+const rootElement =
+  document.getElementById('root') ||
+  document.createElement('div');
+
+function render() {
+  ReactDOM.render(e(App), rootElement);
+}
+
+render();

ba271d3374b66b3ff9736f7c63ac41a29d67935e
Maksim Ryzhikov <rv.maksim@gmail.com>
2017-04-18 12:11:30 +0300

    chore: minimal electron configuration

---
+++ renderer.js
@@ -0,0 +1,6 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+console.log('We are using Node.js ', process.versions.node);
+console.log('Chromium ', process.versions.chrome);
+console.log('and Electron ', process.versions.electron);
